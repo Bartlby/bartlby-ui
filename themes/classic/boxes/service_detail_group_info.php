63f094c2e846886fc6cb4d06bdee5b2d0cdce27e
hjanuschka <helmut@januschka.com>
2012-12-01 23:24:13 +0100

    linked to servicegroup_detail from service_detail

--- themes/classic/boxes/service_detail_group_info.php
+++ themes/classic/boxes/service_detail_group_info.php
@@ -9,7 +9,7 @@ Member of following ServiceGroups:
 echo "<tr><td>";
 for($x=0; $x<count($plcs[service_groups]); $x++) {
 
-	echo "<b>" . $plcs[service_groups][$x][servicegroup_name] . ",</b>";
+	echo "<b><a href='servicegroup_detail.php?servicegroup_id=" . $plcs[service_groups][$x][servicegroup_id] . "'>" . $plcs[service_groups][$x][servicegroup_name] . "</A>,</b>";
 	
 }
 echo "</td></tr>";

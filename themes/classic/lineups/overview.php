65dd098b50e7b0bf1a90d3d478eef5b9488f22c5
Helmut Januschka <helmut@januschka.com>
2012-12-02 12:24:29 +0100

    removed ServerGroups Extension - Server List now uses servergroup tech.

--- themes/classic/lineups/overview.php
+++ themes/classic/lineups/overview.php
@@ -2,7 +2,8 @@
 <?=$this->disp_box("core_info")?>
 <?=$this->disp_box("tactical_overview")?>
 <?=$this->disp_box("system_health")?>
-<?=$this->disp_box("extension_ServerGroups")?>
+<?=$this->disp_box("server_groups")?>
+<?=$this->disp_box("service_groups")?>
 <?=$this->disp_box("UNPLACED")?>
 
 

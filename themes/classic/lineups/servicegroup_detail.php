10ec8ea20fc28a16f54918953f56cc2142dc89cb
Helmut Januschka <helmut@januschka.com>
2012-12-02 11:53:08 +0100

    removed servergroup extension - added it to core-ui

--- themes/classic/lineups/servicegroup_detail.php
+++ themes/classic/lineups/servicegroup_detail.php
@@ -1,4 +1,22 @@
 <?=$this->disp_box("MAIN")?>
 <?=$this->disp_box("servicegroup_detail_servicegroup_info")?>
 <?=$this->disp_box("servicegroup_detail_members")?>
+
+<table class="table table-striped table-bordered " id='services_table'>
+						  <thead>
+							  <tr>
+							  	<th>Server</th>
+								  <th>State</th>
+								  <th>LastCheck</th>
+								  <th>NextCheck</th>
+								  <th>Service</th>
+								  <th>Output</th>
+								  <th>Options</th>
+							  </tr>
+						  </thead>
+						    <tbody>
+<?=$this->disp_box("server_box_*")?>
+
+</tbody>
+</table>
 <?=$this->disp_box("UNPLACED")?>

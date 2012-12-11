229cf54c3b40940cceb2684b389f1b3544118bfe
hjanuschka <helmut@januschka.com>
2012-12-11 11:16:06 +0100

    downtime modify list uses calendar now

--- downtime_list.php
+++ downtime_list.php
@@ -8,45 +8,76 @@ $layout= new Layout();
 $layout->setTitle("Select Downtime");
 $layout->set_menu("downtimes");
 $layout->Form("fm1", $_GET[script]);
-$layout->Table("100%");
+
 
 $r=bartlby_downtime_map($btl->CFG);
 $optind=0;
 for($x=0; $x<count($r); $x++) {
 	if($r[$x][downtime_type] == 1) {
-		$dttype="Service";	
+		$dttype="Service";
+		$cl = "grey";	
 	} else if($r[$x][downtime_type] == 2) {
-		$dttype="Server&nbsp;";
+		$dttype="Server";
+		$cl = "orange";
+	}else if($r[$x][downtime_type] == 3) {
+		$dttype="ServerGroup";
+		$cl = "pink";
+	}else if($r[$x][downtime_type] == 2) {
+		$dttype="ServiceGroup";
+		$cl = "green";
 	}
 	$dts[$optind][c]="";
 	$dts[$optind][v]=$r[$x][downtime_id];	
 	$dts[$optind][k]=date("d.m.Y H:i", $r[$x][downtime_from]) . "&nbsp;&nbsp;-&nbsp;&nbsp;" . date("d.m.Y H:i", $r[$x][downtime_to]) . " " . $r[$x][downtime_notice];
+	
+	
+	$y_from = date("Y", $r[$x][downtime_from]);
+	$m_from = date("m", $r[$x][downtime_from])-1;
+	$d_from = date("d", $r[$x][downtime_from]);
+	$h_from = date("H", $r[$x][downtime_from]);
+	$i_from = date("i", $r[$x][downtime_from]);
+	
+	$y_to = date("Y", $r[$x][downtime_to]);
+	$m_to = date("m", $r[$x][downtime_to])-1;
+	$d_to = date("d", $r[$x][downtime_to]);
+	$h_to = date("H", $r[$x][downtime_to]);
+	$i_to = date("i", $r[$x][downtime_to]);
+	
+	
+	
+	$not = date("d.m.Y H:i", $r[$x][downtime_from]) . "-" . date("d.m.Y H:i", $r[$x][downtime_to]);
+	$not = $dttype . $r[$x][downtime_notice];
+	$evnts .= "
+		event = new Object();
+		event.title = '" . $not . "'; // this should be string
+		event.start = new Date(" . $y_from . "," . $m_from . "," . $d_from . ", " . $h_from . ", " . $i_from . "); // this should be date object
+		event.end = new Date(" . $y_to . "," . $m_to . "," . $d_to . ", " . $h_to . ", " . $i_to . ");
+		event.color = '" . $cl . "';
+		event.url = 'modify_downtime.php?downtime_id=" . $r[$x][downtime_id] . "';
+		event.allDay = false;
+		events.push(event);
+	
+	";
+	
+	
 	$optind++;	
 }
 
-$layout->Tr(
-	$layout->Td(
-			Array(
-				0=>"Downtime:",
-				1=>$layout->DropDown("downtime_id", $dts)
-			)
-		)
+$layout->OUT .= "<div id=calendar style='width: 50%;float:left;'></div>";
 
-);
-$layout->Tr(
-	$layout->Td(
-			Array(
-				0=>Array(
-					'colspan'=> 2,
-					"align"=>"right",
-					'show'=>$layout->Field("Subm", "submit", "next->")
-					)
-			)
-		)
 
-);
 
-$layout->TableEnd();
+
+
+
+
 $layout->FormEnd();
 $layout->display();
+
+echo "<script>
+function addToCalendar() {
+var events = new Array();
+event = new Object();";
+echo $evnts;
+echo "$('#calendar').fullCalendar('addEventSource',events);}</script>";
 ?>

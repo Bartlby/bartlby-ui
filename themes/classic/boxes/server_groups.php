8b210e7bd0dc36fe11453f323b21ff50e8b87af8
hjanuschka <helmut@januschka.com>
2013-01-18 10:53:09 +0100

    skip empty server groups (wich hides DEFAULT group if needed)

--- themes/classic/boxes/server_groups.php
+++ themes/classic/boxes/server_groups.php
@@ -7,7 +7,9 @@
 $c=0;
 	for($x=0; $x<count($plcs[groups]); $x++) {
 		$grp = $plcs[groups][$x];
-		
+		if($grp[service_sum] == 0) {
+				continue;
+		}
 			echo  "<td align=left valign=top width=300>";					
 			echo  "<table width='100%'><tr><td><b><a href='servergroup_detail.php?servergroup_id=" . $grp[servergroup_id] . "'>" . $grp[servergroup_name] . "</b></td><tr><tr><td ><span class='label " . $grp[lbl] . "'>" . $grp[prozent_float] . " % OK</span></td></tr></table>";
 

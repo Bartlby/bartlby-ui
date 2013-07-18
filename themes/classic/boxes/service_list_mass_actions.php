87d36894b3d3c574a091468729ea1a9e14233943
Helmut Januschka <helmut@januschka.com>
2013-07-18 07:58:05 +0200

    up

--- themes/classic/boxes/service_list_mass_actions.php
+++ themes/classic/boxes/service_list_mass_actions.php
@@ -7,5 +7,7 @@ Perform Action on Selected services<br>
 <button class="btn btn-mini " id="services_bulk_disable_checks"><i class=" icon-ban-circle"></i>Disable Checks</button>&nbsp;
 <button class="btn btn-mini " id="services_bulk_enable_notifys"><i class=" icon-ok-circle"></i>Enable Notifications</button>&nbsp;
 <button class="btn btn-mini " id="services_bulk_disable_notifys"><i class=" icon-ban-circle"></i>Disable Notifications</button>&nbsp;
+<button class="btn btn-mini " id="services_bulk_refresh_table" onClick='window.oTable.fnDraw(false)'><i class=" icon-refresh"></i>Refresh Service Table</button>&nbsp;
+
 	
 

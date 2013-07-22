003e0c121340906e4fcd4ec138c1b40b721953f3
hjanuschka <helmut@januschka.com>
2013-07-22 10:08:47 +0200

    new extension overview

--- xajax_dispatcher.php
+++ xajax_dispatcher.php
@@ -216,16 +216,23 @@ function setWorkerState($worker_id, $worker_state) {
 
 function toggle_extension($ext) {
 	global $layout;
+	//FIXME Change to Button :)
 	$res=new xajaxResponse();
 	$fn = "extensions/" . $ext . ".disabled";
 	if(!file_exists($fn)) {
 		@touch($fn);
 		//enable	
 		$res->AddAssign("extension_img_" . $ext, "src", "themes/" . $layout->theme . "/images/extension_disable.gif");
+		
+		$res->AddAssign("extension_button_" . $ext, "className", "btn btn-mini btn-danger");
+		$res->AddAssign("extension_button_" . $ext, "innerHTML", "Disabled");
 		//$res->AddAssign("extension_img_" . $ext, "title", "enable extension");
 	} else {
 		@unlink($fn);
 		$res->AddAssign("extension_img_" . $ext, "src", "themes/" . $layout->theme . "/images/extension_enable.gif");
+		
+		$res->AddAssign("extension_button_" . $ext, "className", "btn btn-mini btn-success");
+		$res->AddAssign("extension_button_" . $ext, "innerHTML", "Enabled");
 		//$res->AddAssign("extension_img_" . $ext, "title", "disable extension");
 		//disable extension_disable.gif
 	}

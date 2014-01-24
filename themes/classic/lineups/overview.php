20feabf4f20df69f87f8dc7fed79e4452c4285ee
Helmut Januschka <helmut@januschka.com>
2014-01-24 20:33:32 +0100

    tabs

--- themes/classic/lineups/overview.php
+++ themes/classic/lineups/overview.php
@@ -1,3 +1,12 @@
+<!--
+<ul class="nav nav-tabs" id="myTab"><li><a href="#tab0">RRD Browser</a></li><li><a href="#tab1">1h</a></li><li><a href="#tab2">24h</a></li><li><a href="#tab3">30 days</a></li><li><a href="#tab4">365 days</a></li></ul>
+
+<div id="myTabContent" class="tab-content">
+<div class="tab-pane" id="tab0">a</div>
+<div class="tab-pane" id="tab1">b</div>
+</div>
+
+
 <div id="system_health_ajax" style='width: 50%; float:right; '>
 <?=$this->disp_box("system_health")?>
 </div>
@@ -13,7 +22,7 @@
 
 
 
-<div style='clear:both' />
+<div style='clear:both'> </div>
 
 <div id="server_groups_ajax">
 <?=$this->disp_box("server_groups")?>
@@ -25,5 +34,38 @@
 <div id="UNPLACED_ajax">
 <?=$this->disp_box("UNPLACED")?>
 </div>
+
+
+-->
+
+	<div id="system_health_ajax" style='width: 50%; float:right; '>
+	<?=$this->disp_box("system_health")?>
+	</div>
+	<div id="core_info_ajax" style='width: 50%; float:left;'>
+	<?=$this->disp_box("core_info")?>
+	</div>
+	<div id="tactical_overview_ajax" style='width: 50%; float:right'>
+	<?=$this->disp_box("tactical_overview")?>
+	</div>
+
+
+
+
+
+
+	<div style='clear:both' ></div>
+
+	<div id="server_groups_ajax">
+	<?=$this->disp_box("server_groups")?>
+	</div>
+	<div id="service_groups_ajax">
+	<?=$this->disp_box("service_groups")?>
+	</div>
+<div id="UNPLACED_ajax">
+	<?=$this->disp_box("UNPLACED")?>
+	</div>
+
+
+
 
 

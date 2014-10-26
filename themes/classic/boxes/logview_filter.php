d97f181fb1fb0441433378219baf5ebc4f172a6c
Helmut Januschka <helmut@januschka.com>
2014-10-26 07:29:52 +0100

    tt33

--- themes/classic/boxes/logview_filter.php
+++ themes/classic/boxes/logview_filter.php
@@ -57,10 +57,12 @@
 		if($plcs[FILTER][service_id] != "") {
 			$def=bartlby_get_service_by_id($btl->RES, $plcs[FILTER][service_id]);
 			$svcM .="<h5>Service Filter:</h5>" . $def[server_name] . "/" . $def[service_name] . "<br>" . $btl->getServiceOptions($def, $layout) . "<a href='service_detail.php?service_id=" . $def[service_id] . "'>Detail</A>";
+			$svcM .= $layout->Field("service_id", "hidden", $def[service_id]);
 		}
 		if($plcs[FILTER][server_id] != "") {
 			$def=bartlby_get_server_by_id($btl->RES, $plcs[FILTER][server_id]);
 			$svcM .="<h5>Server Filter:</h5>" . $def[server_name]  . "<br>" . $btl->getServerOptions($def, $layout) . "<a href='server_detail.php?server_id=" . $def[server_id] . "'>Detail</A>";
+			$svcM .= $layout->Field("server_id", "hidden", $def[server_id]);
 		}
 		if($plcs[FILTER][servergroup_id] != "") {
 			$def = array();
@@ -77,6 +79,7 @@
 
 			
 			$svcM .="<h5>Servergroup Filter:</h5>" . $def[servergroup_name]  . "<br>" . $btl->getServerGroupOptions($def, $layout) . "<a href='servergroup_detail.php?servergroup_id=" . $def[servergroup_id] . "'>Detail</A>";
+			$svcM .= $layout->Field("servergroup_id", "hidden", $def[servergroup_id]);
 		}
 		if($plcs[FILTER][servicegroup_id] != "") {
 			$def = array();
@@ -93,7 +96,7 @@
 
 			
 			$svcM .="<h5>Servicegroup Filter:</h5>" . $def[servicegroup_name]  . "<br>" . $btl->getServiceGroupOptions($def, $layout) . "<a href='servicegroup_detail.php?servicegroup_id=" . $def[servicegroup_id] . "'>Detail</A>";
-		}
+		}	$svcM .= $layout->Field("servicegroup_id", "hidden", $def[servicegroup_id]);
 
 
 

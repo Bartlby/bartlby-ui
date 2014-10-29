fd6a6ce253cb6e4a11676b0f6aceda43c494d0bf
Helmut Januschka <helmut@januschka.com>
2014-10-29 14:29:18 +0000

    test40

--- api/BTL_API.php
+++ api/BTL_API.php
@@ -3,8 +3,6 @@
 class BTL_API {
     function add_service($btl, $options) {
        $options_array = json_decode($options, true);
-
-     
        $rtc=bartlby_add_service($btl->RES, $options_array);
        if(!$rtc) {
        		$rtc =-1;
@@ -13,8 +11,6 @@ class BTL_API {
     }
     function modify_service($btl, $svc_id, $options) {
        $options_array = json_decode($options, true);
-
-     
        $rtc=bartlby_modify_service($btl->RES,  $svc_id, $options_array);
        if(!$rtc) {
        		$rtc =-1;
@@ -22,9 +18,6 @@ class BTL_API {
        return $rtc;
     }    
     function delete_service($btl, $svc_id) {
-       $options_array = json_decode($options, true);
-
-     
        $rtc=bartlby_delete_service($btl->RES,  $svc_id);
        if(!$rtc) {
        		$rtc =-1;

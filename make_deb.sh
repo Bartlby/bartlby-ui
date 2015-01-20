732f81b9c5ee4e179b94f215cecf296d65e7dac3
Helmut Januschka <helmut@januschka.com>
2015-01-20 14:23:09 +0100

    clean git in ui deb

--- make_deb.sh
+++ make_deb.sh
@@ -3,5 +3,6 @@ echo -n "Version:";
 read n
 rm -vfr /tmp/debian-ui/
 php make_deb.php $n;
+rm -vfr /tmp/debian-ui/.git/
 dpkg -b /tmp/debian-ui/ bartlby-ui_$n.deb
 echo "DONE DEB: bartlby-ui_$n.deb"
